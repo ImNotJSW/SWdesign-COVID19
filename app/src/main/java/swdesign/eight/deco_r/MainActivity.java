614393b596c53d4a681654dfc076e09e230131e5
bae1776 <bae1776@gmail.com>
2020-12-15 23:00:51 +0900

    github 수정 시도하기

---
+++ app/src/main/java/swdesign/eight/deco_r/MainActivity.java
@@ -0,0 +1,18 @@
+package swdesign.eight.deco_r;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        int hello = 3;
+        int hdfijeo = 3;
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
