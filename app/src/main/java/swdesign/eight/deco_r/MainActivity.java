6745eb44f279a4d911eed7a607f1526c6fd47f25
bae1776 <bae1776@gmail.com>
2020-12-15 23:16:06 +0900

    layout added

--- app/src/main/java/swdesign/eight/deco_r/MainActivity.java
+++ app/src/main/java/swdesign/eight/deco_r/MainActivity.java
@@ -10,7 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
 
         int hello = 3;
-        int hdfijeo = 3;
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
