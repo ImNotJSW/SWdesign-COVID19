645544043a7bdaeda16c2483bdfc7301648b7f7c
dack-c <qpxmaos3845@gmail.com>
2020-12-16 18:23:17 +0900

    MainActivity.java, acitivity_main.xml을 test~로 이름 바꿈.

--- app/src/main/java/swdesign/eight/deco_r/TestChangeAddressActivity.java
+++ app/src/main/java/swdesign/eight/deco_r/TestChangeAddressActivity.java
@@ -10,12 +10,12 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class TestChangeAddressActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.test_changeaddress_activity);
 
         final Geocoder geocoder = new Geocoder(this);
         final Button changeButton = findViewById(R.id.changeButton);
